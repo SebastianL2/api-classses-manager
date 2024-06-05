ce8641b88e3ed652274d9d7d7da0025f03f48935
SebastianL2 <sebas16cely@gmail.com>
2024-06-05 10:29:28 -0500

    sd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'API Rest Classes Managment'+ process.env.TYPE;
+    return 'API Rest Classes Managment';
   }
 }
