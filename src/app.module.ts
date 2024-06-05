802e022df0bc60be381d8f82f70247d810365541
SebastianL2 <sebas16cely@gmail.com>
2024-06-05 10:46:21 -0500

    chsss

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,13 +13,12 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-    type: 'postgres',
-    host:  process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    entities: [__dirname+'/**/*.entity{.ts,.js}'],
+    type: 'mysql',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'classesdb',
     synchronize: true,
     
   }),
