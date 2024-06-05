5cc4a0e6ea6217b72365a11afa164b084603a675
SebastianL2 <sebas16cely@gmail.com>
2024-06-05 12:39:51 -0500

    fix problems

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors();
+
+  app.enableCors({
+    origin: true, 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
