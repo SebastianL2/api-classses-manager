9197f3de91ffcd9f9ea5c57c4c52aa37e6ff696c
SebastianL2 <sebas16cely@gmail.com>
2024-06-05 11:03:40 -0500

    fix bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,11 +14,12 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
     type: 'mysql',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: parseInt(process.env.DB_PORT, 10) || 3306,
-    username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'classesdb',
+    host:  process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [__dirname+'/**/*.entity{.ts,.js}'],
     synchronize: true,
     
   }),
