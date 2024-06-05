05db33c896cc0588920c9aa4736fbfb180d6f128
SebastianL2 <sebas16cely@gmail.com>
2024-06-05 10:27:20 -0500

    config other server sql

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-    type: 'mysql',
+    type: 'postgres',
     host:  process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT, 10),
     username: process.env.DB_USERNAME,
